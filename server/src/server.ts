9f9b1833906c8cb37e4854720410f88e7d1ab486
ThànhCorn <99862284+ThanhCorn@users.noreply.github.com>
2023-08-15 16:31:20 +0700

    change cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -31,6 +31,9 @@ app.use(
   cors({
     origin: ["https://e-commerce-frontend-vert.vercel.app"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    optionsSuccessStatus: 204, // Set the status code for preflight requests
   })
 );
 app.use(cookieParser());

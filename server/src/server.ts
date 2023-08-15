e439f839477fb1697ab2752bd10298857089650c
ThànhCorn <99862284+ThanhCorn@users.noreply.github.com>
2023-08-15 22:54:20 +0700

    vercel json

--- server/src/server.ts
+++ server/src/server.ts
@@ -50,9 +50,6 @@ app.use(`/api/stripe`, stripeRouter);
 app.use(notFound);
 app.use(errorHandler);
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
-});
 // Start the server
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
